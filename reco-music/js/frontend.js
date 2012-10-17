01f4e39321e39c20a8d2f5b7906364cc259afc1b
Ravikiran Janardhana <ravikiran.j.127@gmail.com>
2012-10-17 19:35:56 -0400

    Extracted playlist videos

--- reco-music/js/frontend.js
+++ reco-music/js/frontend.js
@@ -1,21 +1,34 @@
 // Create a YUI sandbox on your page.
 YUI().use('node', 'event', 'io', 'jsonp','jsonp-url','json-parse', 'autocomplete', 'datasource', function (Y) {
+    var keyUpCount = 0;
+    var prevSearchTerm = '';
+    var renderPlaylistFlag = 0;
     /*
      * handleKeyUp - handle key up event for search text field
      *
      */
-    handleKeyUp =  function(e){
+    var handleKeyUp =  function(e){
         //Stop event
         e.preventDefault();
         var searchTerm = this.get('value');
-        getAutoCompleteResults(searchTerm);
+
+        //Increment counter
+        keyUpCount++;
+        if(keyUpCount > 100){
+            keyUpCount = 1;
+        }
+        //Save some requests by sending requests for every 2 keypress events 
+        if(keyUpCount % 2 == 0 && prevSearchTerm !== searchTerm){
+            getAutoCompleteResults(searchTerm);
+            prevSearchTerm = searchTerm;
+        }
     };
 
     /*
      * getAutoCompleteResults - fetch auto completion results
      *
      */
-    getAutoCompleteResults = function(keyword){
+    var getAutoCompleteResults = function(keyword){
         //Use Y.jsonp for cross-domain requests and Y.io for same domain requests !!!
         var query = encodeURIComponent(keyword);
         var uri = 'http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&jsonp=window.getAutoCompleteResultsSuccess&q='+query;
@@ -23,7 +36,7 @@ YUI().use('node', 'event', 'io', 'jsonp','jsonp-url','json-parse', 'autocomplete
     };
 
     /*
-     * getAutoCompleteResultsSuccess - success handler for getAutoCompleteResults
+     * getAutoCompleteResultsSuccess - success handler for getAutoCompleteResults (Global -> window.getAutoCompleteResultsSuccess)
      *
      */
     getAutoCompleteResultsSuccess = function(response){
@@ -43,7 +56,7 @@ YUI().use('node', 'event', 'io', 'jsonp','jsonp-url','json-parse', 'autocomplete
      * getAutoCompleteResultsFailure - failure handler for getAutoCompleteResults
      *
      */
-    getAutoCompleteResultsFailure = function(id, response, args){
+    var getAutoCompleteResultsFailure = function(id, response, args){
         Y.one('#error').setStyle('display', 'block')
     };
 
@@ -51,7 +64,7 @@ YUI().use('node', 'event', 'io', 'jsonp','jsonp-url','json-parse', 'autocomplete
      * getYTSearchResults - get YouTube search results for a keyword
      *
      */
-    getYTSearchResults = function(keyword){
+    var getYTSearchResults = function(keyword){
         var query = encodeURIComponent(keyword);
         var uri = 'https://gdata.youtube.com/feeds/api/videos?q='+query+'&alt=json&max-results=10&v=2';
         var cfg = {
@@ -70,7 +83,7 @@ YUI().use('node', 'event', 'io', 'jsonp','jsonp-url','json-parse', 'autocomplete
      * getYTSearchResultsSuccess - success handler for getYTSearchResults
      *
      */
-    getYTSearchResultsSuccess = function(id, o, args){
+    var getYTSearchResultsSuccess = function(id, o, args){
         if(!o && !o.responseText){
             Y.one('#status').setContent('Failed to get valid response');
             return;
@@ -87,6 +100,13 @@ YUI().use('node', 'event', 'io', 'jsonp','jsonp-url','json-parse', 'autocomplete
                 iframeHTML = '<iframe id="ytplayer" type="text/html" width="450" height="350" src="http://www.youtube.com/embed/'+videoID+'?autoplay=1" frameborder="0"/>';
                 Y.one('#video-player').setContent(iframeHTML);
                 Y.one('#video-title').setContent(title);
+                Y.one('#right').setStyle('display', 'block');
+                Y.one('#recommend-button').setStyle('display', 'block');
+
+                if(!renderPlaylistFlag){
+                    renderPlaylistFlag = 1;
+                    renderPlaylist();
+                }
             }
         }
     };
@@ -95,8 +115,38 @@ YUI().use('node', 'event', 'io', 'jsonp','jsonp-url','json-parse', 'autocomplete
      * getYTSearchResultsFailure - failure handler for getYTSearchResults
      *
      */
-    getYTSearchResultsFailure = function(id, response, args){
+    var getYTSearchResultsFailure = function(id, response, args){
         Y.one('#status').setContent('Failed to get valid response');
+    };
+
+    var renderPlaylist = function(){
+       //Use Y.jsonp for cross-domain requests and Y.io for same domain requests !!!
+        var uri = 'http://gdata.youtube.com/feeds/api/playlists/PLcN58y5aSltRPvPi3m8Yw2F_NXev2Xqq7?v=2&alt=json&jsonp=window.getPlaylistVideoSuccess'
+        Y.jsonp(uri, getPlaylistVideoSuccess);
+    };
+
+    //Global
+    getPlaylistVideoSuccess = function(response){
+       if(!response){
+            Y.one('#error').setStyle('display', 'block')
+            return;
+       }
+       console.log(response);
+       if(response.feed && response.feed.entry){
+           var videoArr = response.feed.entry;
+           var iframeHTML = '';
+           for(var i = 0; i < videoArr.length; i++){
+               if(videoArr[i].media$group){
+                   var video = videoArr[i].media$group;
+                   if(video.media$title && video.media$title.$t && video.yt$videoid && video.yt$videoid.$t){
+                       title = video.media$title.$t;
+                       videoID = video.yt$videoid.$t;
+                       iframeHTML = '<div id="playlist-vid"><iframe id="ytplayer" type="text/html" width="200" height="120" src="http://www.youtube.com/embed/'+videoID+'?" frameborder="0"/></div>';
+                       Y.one('#video-playlist').append(iframeHTML);
+                    }
+               }
+           }
+       }
     };
 
     var sKeyword = Y.one('#keyword');
